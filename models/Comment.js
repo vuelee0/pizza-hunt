39fe113f60ac1ff7ffec2373253c1753a8bc4c0d
Vue Lee <vuelee09@gmail.com>
2022-07-08 12:12:52 -0500

    add reply functionality

--- models/Comment.js
+++ models/Comment.js
@@ -1,6 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
 
-const CommentSchema = new Schema({
+const ReplySchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        replyId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        replyBody: {
+            type: String
+        },
+        writtenBy: {
+            type: String
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+          getters: true
+        }
+    }
+);
+
+const CommentSchema = new Schema(
+    {
     writtenBy: {
         type: String
     },
@@ -9,10 +37,26 @@ const CommentSchema = new Schema({
     },
     createdAt: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
-});
+);
 
 const Comment = model('Comment', CommentSchema);
+
+// get total count of comments and replies on retrieval
+CommentSchema.virtual('replyCount').get(function() {
+    return this.replies.length;
+});
 
 module.exports = Comment;
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, removeComment, addReply, removeReply } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 router
@@ -9,6 +9,11 @@ router
 // /api/comments/<pizzaId>/<commentId>
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment);
+
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
 
 module.exports = router;
